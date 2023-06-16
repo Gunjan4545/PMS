520a5a9e70c806723a59f887a8d5d5dc9d6308e8
Gunjan Chaudhari <112699881+Gunjan4545@users.noreply.github.com>
2023-06-16 17:51:23 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,12 +4,10 @@ const mongoose = require("mongoose");
 //connecting to the database
 mongoose.connect(
  
-  // "mongodb+srv://admin:admin123@cluster0.ljmczch.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://admin:admin123@cluster0.ljmczch.mongodb.net/?retryWrites=true&w=majority"
   
-"mongodb+srv://bhaveshc1509:wQR7FVGPKIa0nP5d@cluster0.1ggifzf.mongodb.net/mpr?retryWrites=true&w=majority"
-  // "mongodb+srv://18minorpr:RSFJ0XHp7NxxtiRK@cluster0.ui5tb2s.mongodb.net/"
-  // RSFJ0XHp7NxxtiRK
-
+// "mongodb+srv://bhaveshc1509:wQR7FVGPKIa0nP5d@cluster0.1ggifzf.mongodb.net/mpr?retryWrites=true&w=majority"
+  
   // "mongodb://127.0.0.1:27017/mpr"
 );
 
